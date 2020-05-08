c9669371a87770c1951ba290f2daca8db7b0eabf
srvaz <jonwify@gmail.com>
2020-05-08 13:03:13 -0300

    :fire: Remove unnecessary code

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,3 @@
-export * from './ping.controller';
-export * from './menu.controller';
-export * from './stock.controller';
 export * from './order.controller';
+export * from './stock.controller';
+
